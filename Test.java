9c42e1f461f60fde8b5660537750cbbb9d4a1fb3
ingalevaishnavi2003 <ingalevaishnavi521@gmail.com>
2026-03-14 10:55:40 +0530

    Create Test.java
    Successfully run

---
+++ Test.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class Test{
+	
+	public static void main (String args[])
+	{
+		int n=6;
+		for(int i=1;i<=n;i++) {
+		
+		for(int j=0;j<i;j++)
+		{
+			System.out.print(((i+j)%2)+" ");
+
+		}
+		System.out.println(" ");
+
+		
+	}
+	}
+
+}
